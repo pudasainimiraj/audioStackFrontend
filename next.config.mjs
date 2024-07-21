2b8976d6be876e58276058023f010caae8708b25
klokMiraj <pudasainimetal96@gmail.com>
2024-07-21 01:11:33 +0100

    Deploy Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,31 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default withBundleAnalyzer({
-  eslint: {
-    dirs: ["."]
-  },
+  reactStrictMode: true,
   poweredByHeader: false,
   trailingSlash: false,
-  reactStrictMode: true,
   images: {
-    domains: [`${process.env.NEXT_PUBLIC_BASE_URL}`]
+    domains: [process.env.NEXT_PUBLIC_BASE_URL],
   },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@audio_stack_frontend": path.resolve(__dirname)
+      "@audio_stack_frontend": path.resolve(__dirname),
     };
     return config;
   },
-   env: {
+  env: {
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
     REACT_APP_API_SECRET: process.env.REACT_APP_API_SECRET,
   },
+  output: 'export',
 });
-
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-};
-
-module.exports = withBundleAnalyzer(nextConfig);
