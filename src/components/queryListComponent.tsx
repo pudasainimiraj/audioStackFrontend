83490ca9566fd0d628456b0e2d5721d9c0b55d05
klokMiraj <pudasainimetal96@gmail.com>
2024-07-21 14:20:16 +0100

    Build fix

--- src/components/queryListComponent.tsx
+++ src/components/queryListComponent.tsx
@@ -33,64 +33,65 @@ const ArtistsList = () => {
     if (pagination && pagination.page < pagination.pages) {
       // fetchData(pagination.search, pagination.page + 1);
     }
-  };
 
-  if (loading) {
+
+    if (loading) {
+      return (
+        <Box textAlign="center" p={5}>
+          <Spinner size="xl" />
+        </Box>
+      );
+    }
+
+    if (type !== "artist") {
+      return (
+        <Box textAlign="center" p={5}>
+          <Text fontSize="lg">No artist data available</Text>
+        </Box>
+      );
+    }
+
     return (
-      <Box textAlign="center" p={5}>
-        <Spinner size="xl" />
-      </Box>
-    );
-  }
-
-  if (type !== "artist") {
-    return (
-      <Box textAlign="center" p={5}>
-        <Text fontSize="lg">No artist data available</Text>
-      </Box>
-    );
-  }
-
-  return (
-    <Box>
-      <SimpleGrid
-        columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
-        spacing={5}
-        p={5}
-        paddingTop={15}
-      >
-        {results.map((artist) => (
-          <Box
-            key={artist.id}
-            onClick={() => handleCardClick(artist.id)}
-            cursor="pointer"
-            width="250px"
-            height="350px"
-            overflow="hidden"
+      <Box>
+        <SimpleGrid
+          columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
+          spacing={5}
+          p={5}
+          paddingTop={15}
+        >
+          {results.map((artist) => (
+            <Box
+              key={artist.id}
+              onClick={() => handleCardClick(artist.id)}
+              cursor="pointer"
+              width="250px"
+              height="350px"
+              overflow="hidden"
+            >
+              <GenericCard artist={artist} />
+            </Box>
+          ))}
+        </SimpleGrid>
+        <Flex justifyContent="center" mt="4">
+          <Button
+            onClick={handlePrevPage}
+            disabled={pagination.page <= 1}
+            leftIcon={<ChevronLeftIcon />}
+            mr="2"
           >
-            <GenericCard artist={artist} />
-          </Box>
-        ))}
-      </SimpleGrid>
-      <Flex justifyContent="center" mt="4">
-        <Button
-          onClick={handlePrevPage}
-          disabled={pagination.page <= 1}
-          leftIcon={<ChevronLeftIcon />}
-          mr="2"
-        >
-          Prev
-        </Button>
-        <Button
-          onClick={handleNextPage}
-          disabled={pagination.page >= pagination.pages}
-          rightIcon={<ChevronRightIcon />}
-        >
-          Next
-        </Button>
-      </Flex>
-    </Box>
-  );
-};
+            Prev
+          </Button>
+          <Button
+            onClick={handleNextPage}
+            disabled={pagination.page >= pagination.pages}
+            rightIcon={<ChevronRightIcon />}
+          >
+            Next
+          </Button>
+        </Flex>
+      </Box>
+    );
+  };
+}
 
 export default ArtistsList;
